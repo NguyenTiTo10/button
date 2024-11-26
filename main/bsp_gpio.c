048e7b617740d22a6083002f73b3ca64923930a0
nguyentito10 <tientoan91002003@gmail.com>
2024-11-26 22:13:11 +0700

    Completed function bsp_gpio_write_pin

--- main/bsp_gpio.c
+++ main/bsp_gpio.c
@@ -5,12 +5,12 @@ uint8_t bsp_gpio_read_pin  (gpio_num_t gpio_num)
   return gpio_get_level(gpio_num);
 }
 
-void bsp_gpio_toggle_pin  (gpio_num_t gpio_num)
+void bsp_gpio_write_pin   (gpio_num_t gpio_num, uint32_t level)
 {
-
+  gpio_set_level(gpio_num, level);
 }
 
-void bsp_gpio_write_pin   (gpio_num_t gpio_num, uint32_t level)
+void bsp_gpio_toggle_pin  (gpio_num_t gpio_num)
 {
   
 }
