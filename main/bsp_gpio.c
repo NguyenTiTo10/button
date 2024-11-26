bbb9b60a272028242ac5fb32f30b501e2bc1961f
nguyentito10 <tientoan91002003@gmail.com>
2024-11-26 22:11:12 +0700

    Completed function bsp_gpio_read_pin

--- main/bsp_gpio.c
+++ main/bsp_gpio.c
@@ -1,9 +1,16 @@
 #include "bsp_gpio.h"
 
-uint16_t bsp_gpio_read_pin  (gpio_num_t gpio_num)
+uint8_t bsp_gpio_read_pin  (gpio_num_t gpio_num)
+{
+  return gpio_get_level(gpio_num);
+}
+
+void bsp_gpio_toggle_pin  (gpio_num_t gpio_num)
+{
+
+}
+
+void bsp_gpio_write_pin   (gpio_num_t gpio_num, uint32_t level)
 {
   
 }
-
-void bsp_gpio_toggle_pin  (gpio_num_t gpio_num);
-void bsp_gpio_write_pin   (gpio_num_t gpio_num, uint32_t level); 
