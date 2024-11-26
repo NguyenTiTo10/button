d4d9fb7750269d75f9477756f1df8a60f2a50718
nguyentito10 <tientoan91002003@gmail.com>
2024-11-26 23:57:13 +0700

    Update function check state of button

--- main/drv_button.c
+++ main/drv_button.c
@@ -1,8 +1,12 @@
 #include "drv_button.h"
 
+#define TAG "Button ISR"          // Define a tag for logging
 #define DEBOUNCE_DELAY_MS 50     // Debounce delay in milliseconds
 
 static volatile bool isr_flag = false;
+static uint32_t last_debounce_time = 0;     // Last debounce time
+// static bool last_button_state = false;      // Last stable state of the button (true = pressed, false = not pressed)
+
 
 bool button_isr_flag ()
 {
@@ -13,6 +17,18 @@ void update_button_state (void)
 {
   // Get the current time in milliseconds (use your preferred method to get time)
   uint32_t current_time = bsp_timer_get_time() / 1000; // Convert to milliseconds
+
+  if (current_time - last_debounce_time >= DEBOUNCE_DELAY_MS) 
+  {
+    last_debounce_time = current_time;
+
+    if (isr_flag) 
+      ESP_LOGI(TAG, "Button Pressed!");
+    else 
+      ESP_LOGI(TAG, "Button Released!");
+
+    isr_flag = false;
+  }
 }
 
 
--- main/drv_button.h
+++ main/drv_button.h
@@ -7,8 +7,7 @@
 #include "bsp_gpio.h"
 #include "bsp_timer.h"
 
-#define BUTTON_GPIO GPIO_NUM_0   // Use GPIO0 for the button
-#define TAG "Button ISR"  // Define a tag for logging
+#define BUTTON_GPIO GPIO_NUM_0    // Use GPIO0 for the button
 
 void IRAM_ATTR button_isr_handler(void *arg);
 
