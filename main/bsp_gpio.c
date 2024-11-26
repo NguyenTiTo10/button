982f47e04d72f19efb11a9813466746e461cb86a
nguyentito10 <tientoan91002003@gmail.com>
2024-11-26 22:09:17 +0700

    Refactor: change the type of value function bsp gpio read pin

---
+++ main/bsp_gpio.c
@@ -0,0 +1,9 @@
+#include "bsp_gpio.h"
+
+uint16_t bsp_gpio_read_pin  (gpio_num_t gpio_num)
+{
+  
+}
+
+void bsp_gpio_toggle_pin  (gpio_num_t gpio_num);
+void bsp_gpio_write_pin   (gpio_num_t gpio_num, uint32_t level); 
--- main/bsp_gpio.h
+++ main/bsp_gpio.h
@@ -5,7 +5,7 @@
 #include "esp_system.h"
 #include "esp_log.h"
 
-void bsp_gpio_read_pin    (gpio_num_t gpio_num);
+uint8_t bsp_gpio_read_pin    (gpio_num_t gpio_num);
 void bsp_gpio_toggle_pin  (gpio_num_t gpio_num);
 void bsp_gpio_write_pin   (gpio_num_t gpio_num, uint32_t level); 
 
