3c4112409b3de1db169a462e00bd4ec21f034390
nguyentito10 <tientoan91002003@gmail.com>
2024-11-26 23:23:44 +0700

    Refactor the name of bsp timer function

--- main/bsp_timer.h
+++ main/bsp_timer.h
@@ -1,10 +1,12 @@
 #include <stdint.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_timer.h"
 
 #ifndef BSP_TIMER_H
 #define BSP_TIMER_H
 
-void bsp_delay (uint32_t time);
+void bsp_timer_delay    (uint32_t time);
+void bsp_timer_get_time (void);
 
 #endif
