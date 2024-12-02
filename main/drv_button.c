8f79b8f786999529bfcb7abf5bab55d707485eee
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 00:42:01 +0700

    Refactor: rename function drv_btn_detect_press

--- main/drv_button.c
+++ main/drv_button.c
@@ -8,26 +8,37 @@ static  drv_btn_type_t     last_button_press   = NON_BTN_PRESSED;   // Last stab
 
 static uint8_t  count = 1;
 
-drv_btn_type_t drv_detect_btn_press ()
-{
-  if (!bsp_get_isr_flag())
-    return NON_BTN_PRESSED;
-
-  if (bsp_gpio_read_pin(MAIN_BTN_GPIO))
-    return MAIN_BTN_PRESSED;
-  else if (bsp_gpio_read_pin(LEFT_BTN_GPIO))
-    return LEFT_BTN_PRESSED;
-  else if (bsp_gpio_read_pin(MID_BTN_GPIO))
-    return LEFT_BTN_PRESSED;
-  else if (bsp_gpio_read_pin(RIGHT_BTN_GPIO))
-    return RIGHT_BTN_PRESSED;
-}
-
-void update_button_state (void)
+drv_btn_type_t drv_btn_detect_press ()
 {
   while (1)
   {
-    drv_btn_type_t button_press = drv_detect_btn_press();
+    if (!bsp_get_isr_flag())
+      continue;
+    drv_btn_type_t button_press = drv_btn_detect_press();
+
+    if (button_press == NON_BTN_PRESSED)
+    if (bsp_get_isr_flag() && bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
+    {
+      uint32_t current_time = bsp_timer_get_time() / 1000;
+      if ((current_time - last_debounce_time) > DEBOUNCE_DELAY_MS)
+        ESP_EARLY_LOGI(TAG, "Count: %d - Button Pressed!", count);
+      
+      last_debounce_time = current_time;
+      count++;
+      bsp_set_isr_flag (false);
+    }
+
+    bsp_timer_delay(10);
+  }
+}
+
+drv_btn_type_t update_button_state (void)
+{
+  while (1)
+  {
+    if (!bsp_get_isr_flag())
+      continue;
+    drv_btn_type_t button_press = drv_btn_detect_press();
 
     if (button_press == NON_BTN_PRESSED)
     if (bsp_get_isr_flag() && bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
