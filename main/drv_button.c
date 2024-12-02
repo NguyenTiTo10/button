b8845282a76e3a783489c5ed47d8510d1f2dedfe
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 00:58:47 +0700

    Init function drv_btn_update_state

--- main/drv_button.c
+++ main/drv_button.c
@@ -8,9 +8,9 @@ static  drv_btn_type_t     last_button_press   = NON_BTN_PRESSED;   // Last stab
 static  uint32_t           current_time;
 static  uint32_t           elasped_time;
 
-static  uint8_t  count = 1;
+static  uint8_t             count = 1;
 
-drv_btn_type_t drv_btn_detect_press ()
+drv_btn_type_t drv_btn_detect_press (void)
 {
   if (!bsp_get_isr_flag())
     return NON_BTN_PRESSED;
@@ -36,28 +36,23 @@ drv_btn_type_t drv_btn_detect_press ()
 
 
 
-drv_btn_type_t drv_btn_update_state (void)
+void drv_btn_update_state (void)
 {
-  while (1)
-  {
-    if (!bsp_get_isr_flag())
-      continue;
-    drv_btn_type_t button_press = drv_btn_detect_press();
+  drv_btn_type_t btn_state = drv_btn_detect_press();
 
-    if (button_press == NON_BTN_PRESSED)
-    if (bsp_get_isr_flag() && bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
-    {
-      uint32_t current_time = bsp_timer_get_time() / 1000;
-      if ((current_time - last_debounce_time) > DEBOUNCE_DELAY_MS)
-        ESP_EARLY_LOGI(TAG, "Count: %d - Button Pressed!", count);
-      
-      last_debounce_time = current_time;
-      count++;
-      bsp_set_isr_flag (false);
-    }
+  if (btn_state == NON_BTN_PRESSED)
+    return;
+  
+  if (btn_state == MAIN_BTN_PRESSED)
+    ESP_EARLY_LOGI(TAG, "Count: %d - Main Button Pressed!", count);
+  
+  else if (btn_state == LEFT_BTN_PRESSED)
+    ESP_EARLY_LOGI(TAG, "Count: %d - Left Button Pressed!", count);
 
-    bsp_timer_delay(10);
-  }
+  else if (btn_state == RIGHT_BTN_PRESSED)
+    ESP_EARLY_LOGI(TAG, "Count: %d - Left Button Pressed!", count);
+  
+  count += 1;
 }
 
 
