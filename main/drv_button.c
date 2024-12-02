43702dd6a932c947001b84e264db7ccae05cea0a
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 00:28:26 +0700

    Refactor: rename state non pressed button

--- main/drv_button.c
+++ main/drv_button.c
@@ -4,14 +4,14 @@
 #define DEBOUNCE_DELAY_MS 50     // Debounce delay in milliseconds
 
 static  uint32_t           last_debounce_time  = 0;               // Last debounce time
-static  drv_btn_type_t     last_button_press   = NON_BTN_PRESS;   // Last stable state of the button (true = pressed, false = not pressed)
+static  drv_btn_type_t     last_button_press   = NON_BTN_PRESSED;   // Last stable state of the button (true = pressed, false = not pressed)
 
 static uint8_t  count = 1;
 
 drv_btn_type_t drv_detect_btn_press ()
 {
   if (!bsp_get_isr_flag())
-    return NON_BTN_PRESS;
+    return NON_BTN_PRESSED;
 
   if (bsp_gpio_read_pin(MAIN_BTN_GPIO))
     return MAIN_BTN_PRESSED;
@@ -27,6 +27,9 @@ void update_button_state (void)
 {
   while (1)
   {
+    drv_btn_type_t button_press = drv_detect_btn_press();
+
+    if (button_press == NON_BTN_PRESSED)
     if (bsp_get_isr_flag() && bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
     {
       uint32_t current_time = bsp_timer_get_time() / 1000;
