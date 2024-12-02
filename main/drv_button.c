0c81a22366d23871faccb25eb592b3cf211f82f1
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 00:53:22 +0700

    Update: function drv_btn_detect_press

--- main/drv_button.c
+++ main/drv_button.c
@@ -5,31 +5,33 @@
 
 static  uint32_t           last_debounce_time  = 0;               // Last debounce time
 static  drv_btn_type_t     last_button_press   = NON_BTN_PRESSED;   // Last stable state of the button (true = pressed, false = not pressed)
+static  uint32_t           current_time;
+static  uint32_t           elasped_time;
 
-static uint8_t  count = 1;
+static  uint8_t  count = 1;
 
 drv_btn_type_t drv_btn_detect_press ()
 {
-  while (1)
-  {
-    if (!bsp_get_isr_flag())
-      continue;
-    drv_btn_type_t button_press = drv_btn_detect_press();
+  if (!bsp_get_isr_flag())
+    return NON_BTN_PRESSED;
 
-    if (button_press == NON_BTN_PRESSED)
-    if (bsp_get_isr_flag() && bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
-    {
-      uint32_t current_time = bsp_timer_get_time() / 1000;
-      if ((current_time - last_debounce_time) > DEBOUNCE_DELAY_MS)
-        ESP_EARLY_LOGI(TAG, "Count: %d - Button Pressed!", count);
-      
-      last_debounce_time = current_time;
-      count++;
-      bsp_set_isr_flag (false);
-    }
+    // Count the time to check whether debounce or not
+  current_time = bsp_timer_get_time() / 1000;
+  elasped_time = current_time - last_debounce_time;
+  if (elasped_time <= DEBOUNCE_DELAY_MS)
+    return NON_BTN_PRESSED;
 
-    bsp_timer_delay(10);
-  }
+  bsp_set_isr_flag (false);
+  last_debounce_time = current_time;
+
+  if (bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
+    return MAIN_BTN_PRESSED;
+  
+  if (bsp_gpio_read_pin(LEFT_BTN_GPIO) == 1)
+    return LEFT_BTN_PRESSED;
+
+  if (bsp_gpio_read_pin(RIGHT_BTN_GPIO) == 1)
+    return RIGHT_BTN_PRESSED;
 }
 
 drv_btn_type_t update_button_state (void)
